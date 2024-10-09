11f602f929448936e4a9e3403c63e3c87e235a5a
Nicolas Gonzalez <nigonzalez@autentia.cl>
2024-10-08 23:46:13 -0300

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
-import netlify from '@astrojs/netlify/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  adapter: netlify(),
 });
